8d4e29c83a8ef870db7a2b60e08d4f4e78063153
lfal7838 <lashan.faliq@syscolabs.com>
2020-04-13 19:24:51 +0530

    Install axios

--- src/services/authenticationServices.js
+++ src/services/authenticationServices.js
@@ -1,7 +1,18 @@
+import axios from 'axios';
+
 export const signUp = ()=>{
 
 }
 
-export const login = ()=>{
-    
+export const login = ({ email, password })=>{
+    axios.post('/api/login/', {
+        email,
+        password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
