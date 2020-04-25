f45f30125069573d82cf955a540876d360a0611a
Kavin Ranawella <kavinranawella@Kavins-MacBook-Pro.local>
2020-04-25 16:47:38 +0530

    added form

---
+++ src/components/shoppingForm/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+const ShoppingForm = (props) => {
+
+    return (
+        <div className="App-margin">
+            <Form>
+                <FormGroup row>
+                    <Label align="left" for="exampleEmail" sm={ 2 }>Email</Label>
+                    <Col sm={ 10 }>
+                        <Input width="5" type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label align="left"  for="examplePassword" sm={ 2 }>Password</Label>
+                    <Col sm={ 10 }>
+                        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label align="left"  for="exampleSelect" sm={ 2 }>Select</Label>
+                    <Col sm={ 10 }>
+                        <Input type="select" name="select" id="exampleSelect">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label align="left"  for="exampleSelectMulti" sm={ 2 }>Select Multiple</Label>
+                    <Col sm={ 10 }>
+                        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label align="left"  for="exampleText" sm={ 2 }>Text Area</Label>
+                    <Col sm={ 10 }>
+                        <Input type="textarea" name="text" id="exampleText" />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label align="left"  for="exampleFile" sm={ 2 }>File</Label>
+                    <Col sm={ 10 }>
+                        <Input type="file" name="file" id="exampleFile" />
+                        <FormText color="muted">
+                            This is some placeholder block-level help text for the above input.
+                            It's a bit lighter and easily wraps to a new line.
+                        </FormText>
+                    </Col>
+                </FormGroup>
+                <FormGroup tag="fieldset" row>
+                    <legend className="col-form-label col-sm-2">Radio Buttons</legend>
+                    <Col sm={ 10 }>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="radio" name="radio2" />{' '}
+                                Option one is this and that—be sure to include why it's great
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="radio" name="radio2" />{' '}
+                                Option two can be something else and selecting it will deselect option one
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check disabled>
+                            <Label check>
+                                <Input type="radio" name="radio2" disabled />{' '}
+                                Option three is disabled
+                            </Label>
+                        </FormGroup>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="checkbox2" sm={ 2 }>Checkbox</Label>
+                    <Col sm={ { size: 10 } }>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" id="checkbox2" />{' '}
+                                Check me out
+                            </Label>
+                        </FormGroup>
+                    </Col>
+                </FormGroup>
+                <FormGroup check row>
+                    <Col sm={ { size: 10, offset: 2 } }>
+                        <Button>Submit</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
+        </div>
+    );
+}
+
+export default ShoppingForm;
