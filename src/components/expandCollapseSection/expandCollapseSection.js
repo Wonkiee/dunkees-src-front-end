474dc02f10c0b62a1f3926691a00a841f76fe63d
Rajitha Rajasooriya <rajitha.rajasooriya@syscolabs.com>
2020-04-26 00:54:52 -0400

    first commit

---
+++ src/components/expandCollapseSection/expandCollapseSection.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import '../expandCollapseSection/expandCollapseSection.scss';
+
+import constants from '.././../utils/constants';
+
+export default class ExpandCollapseSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.arrowDirection = 'fas fa-angle-double-right';
+        this.state = {
+            isOpen: false,
+        };
+    }
+
+    toggle = () => {
+        if (this.state.isOpen) {
+            this.arrowDirection = constants.ARROW_DIRECTIONS.RIGHT;
+        }
+        else {
+            this.arrowDirection = constants.ARROW_DIRECTIONS.DOWN;
+        }
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div className="expand-collapse-sec">
+                <div onClick={ this.toggle } className="title" id="expandCollapsSec">
+                    <span className="sectionTitle left">
+                        <i className={ `fas fa-angle-double-${ this.arrowDirection }` }></i> { this.props.title }
+                    </span>
+                </div>
+                <Collapse isOpen={ this.state.isOpen }>
+                    <Card>
+                        <CardBody>
+                            Awula => Fuck U <br/>
+                            Gappiya => Fuck U <br/>
+                            Upasaka => Fuck U <br/>
+                            Billa => Fuck u
+                        </CardBody>
+                    </Card>
+                </Collapse>
+            </div>
+        );
+    }
+}
